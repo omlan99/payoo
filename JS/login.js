39f6c14d481384157d68737977c1401646349328
omlan99 <omlan10616@gmail.com>
2024-09-19 23:15:13 +0600

    first commit

---
+++ JS/login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNum = document.getElementById('phone-num').value;
+    const pinNum = document.getElementById('pin-num').value;
+    if(phoneNum === "5" && pinNum === '1234'){
+        console.log('you are logged in')
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('wrong phone num or pin')
+    }
+})
